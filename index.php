6c3bcfd785bd244ccc964f1e9b0f7bf1f954da35
barkalov <barkalovys@gmail.com>
2017-07-26 02:39:44 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register(function($className)
+{
+    $namespace = str_replace("\\","/",__NAMESPACE__);
+    $className = str_replace("\\","/",$className);
+    $class = __DIR__ . "/src/" . (empty($namespace) ? "" : $namespace . "/") . "{$className}.php";
+    include_once($class);
+});
+
+$players = new \Entity\Player\PlayerList(5);
+$game = new \Entity\Game\FiveCardDraw\FiveCardDraw(new \Service\Deck\StandardDeckBuilder(), $players);
+$game->loop();
